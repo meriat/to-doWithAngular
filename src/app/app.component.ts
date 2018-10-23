8ec8ea13c2420f9b3071fd8281a014d695e25298
Mark Mangahas and Meria Thomas <meria@>
2018-10-23 14:05:41 -0700

    add two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './models/task.model';
-import { CloneVisitor } from '@angular/compiler/src/i18n/i18n_ast';
+
 
 @Component({
   selector: 'app-root',
@@ -17,22 +17,29 @@ year: number = this.currentTime.getFullYear();
 tasks: Task[] = [
   new Task('Finish weekend Angular homework for Epicodus course', 2),
   new Task('Begin brainstorming possible JavaScript group projects',3),
-  new Task('Add README file to last few Angular repos on GitHub',3)
+  new Task('Add README file to last few Angular repos on GitHub',0)
   ];
 
-  editTask() {
-    alert("Time to edit a task!");
-  }
+  selectedTask = null;
 
     priorityColor(currentTask){
       if(currentTask.priority === 3){
         return "bg-primary";
       } else if(currentTask.priority === 2) {
         return "bg-success";
+      } else if (currentTask.priority === 1 ){ 
+        return "bg-warning";
       } else {
         return "bg-info";
       }
 
+    }
+
+    editTask(clickedTask) {
+      this.selectedTask = clickedTask;
+    }
+    finishedEditing() {
+      this.selectedTask = null;
     }
   
 }
