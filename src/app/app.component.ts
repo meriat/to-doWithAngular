75773ee50280698136be2cc6be6ce45857d2fe37
Mark Mangahas and Meria Thomas <mark@>
2018-10-23 11:33:12 -0700

    add property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Task } from './models/task.model';
+import { CloneVisitor } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Component({
   selector: 'app-root',
@@ -14,13 +15,25 @@ day: number = this.currentTime.getDate();
 year: number = this.currentTime.getFullYear();
 
 tasks: Task[] = [
-  new Task('Finish weekend Angular homework for Epicodus course'),
-  new Task('Begin brainstorming possible JavaScript group projects'),
-  new Task('Add README file to last few Angular repos on GitHub')
+  new Task('Finish weekend Angular homework for Epicodus course', 2),
+  new Task('Begin brainstorming possible JavaScript group projects',3),
+  new Task('Add README file to last few Angular repos on GitHub',3)
   ];
 
   editTask() {
     alert("Time to edit a task!");
   }
+
+    priorityColor(currentTask){
+      if(currentTask.priority === 3){
+        return "bg-primary";
+      } else if(currentTask.priority === 2) {
+        return "bg-success";
+      } else {
+        return "bg-info";
+      }
+
+    }
+  
 }
 
